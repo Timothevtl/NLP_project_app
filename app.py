38034fea44f759a6eebb0aac1d2329822f3f8737
Timothevtl <115265913+Timothevtl@users.noreply.github.com>
2024-01-23 09:45:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from io import StringIO
 import zipfile
 
-st.cache_data
+st.cache_resource
 def get_english_stopwords():
     nltk.download('stopwords')
     english_stopwords = set(stopwords.words('english'))
