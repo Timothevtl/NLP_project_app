3537c879b59e3a0e6664be3c23881519346e15a4
Timothevtl <115265913+Timothevtl@users.noreply.github.com>
2024-01-24 14:07:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -169,26 +169,14 @@ def interactive_qa_t5(df, new_tfidf_vectorizer, new_tfidf_matrix, qa_pipeline):
 
 # Main
 def main():
-    st.title(":books: Welcome to the NLP Project 2 App : Book Analysis and recommandation models :books:")
+    st.title("Welcome to the NLP Project 2 App")
+    st.title(":books: Book Analysis and recommandation models :books:")
     st.write("Created by Anna ZENOU and Timothe VITAL")
     st.write("Explore book reviews, get recommendations, and ask questions about your favorite books.")
 
     
     st.sidebar.title("Navigation")
     app_mode = st.sidebar.radio("Go to", ["Home", "Sentiment Analysis", "Semantic Search", "Question Answering", "Book recommendation"])
-    st.write("Explore book reviews, get recommendations, and ask questions about your favorite books.")
-    # Add buttons or links to different app sections
-    if st.button("Try our book review sentiment analysis model"):
-        app_mode = "Sentiment Analysis"
-    
-    if st.button("Or get a book recommendation from our top 1000 best books !"):
-        app_mode == "Book recommendation"
-    
-    if st.button("You want to know more about the content of a specific book? Ask our Question Answering model"):
-        app_mode = "Question Answering"
-
-    if st.button("Want to know relation between words? Try the semantic research !"):
-        app_mode = "Semantic Search"
     
     if app_mode == "Book recommendation":
         tfidf_vectorizer_similar_book = TfidfVectorizer(stop_words='english')
